84d74738a3770076ebde69c3c321e1e5b5146b53
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2016-08-03 14:37:46 +0200

    Started adding actual tests for Python modules. Implemented list wrapper around Riemann solver. Added TRRSSolver to Python module

--- src/python/RiemannModule.cpp
+++ src/python/RiemannModule.cpp
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * This file is part of Shadowfax
  * Copyright (C) 2015 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+ *               2016 Bert Vandenbroucke
  *
  * Shadowfax is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -23,12 +24,58 @@
  *
  * @author Bert Vandenbroucke (bert.vandenbroucke@ugent.be)
  */
+#include "riemann/ApproximateSolver.hpp"
 #include "riemann/ExactRiemannSolver.hpp"
+#include "riemann/RiemannSolver.hpp"
 
 #include <boost/python/class.hpp>
 #include <boost/python/def.hpp>
+#include <boost/python/extract.hpp>
+#include <boost/python/list.hpp>
 #include <boost/python/module.hpp>
 using namespace boost::python;
+
+/**
+ * @brief Wrapper around ExactRiemannSolver::solve
+ *
+ * @param solver Active RiemannSolver instance used to solve the Riemann problem
+ * @param WL Python list containing the left state hydrodynamical variables
+ * @param WR Python list containing the right state hydrodynamical variables
+ * @return Python list containing the solution hydrodynamical variables
+ */
+template <typename Solver> list python_solve(Solver& solver, list WL, list WR) {
+
+    double WLarr[ndim_ + 2];
+    for(unsigned int i = 0; i < ndim_ + 2; i++) {
+        WLarr[i] = extract<double>(WL[i]);
+    }
+    double WRarr[ndim_ + 2];
+    for(unsigned int i = 0; i < ndim_ + 2; i++) {
+        WRarr[i] = extract<double>(WR[i]);
+    }
+
+#if ndim_ == 3
+    StateVector WLvec(WLarr[0], WLarr[1], WLarr[2], WLarr[3], WLarr[4]);
+    StateVector WRvec(WRarr[0], WRarr[1], WRarr[2], WRarr[3], WRarr[4]);
+#else
+    StateVector WLvec(WLarr[0], WLarr[1], WLarr[2], WLarr[3]);
+    StateVector WRvec(WRarr[0], WRarr[1], WRarr[2], WRarr[3]);
+#endif
+    Vec normal;
+    normal[0] = 1.;
+    double maxmach;
+    StateVector Whalfvec = solver.solve(WLvec, WRvec, normal, maxmach);
+
+    list Whalf;
+    Whalf.append(Whalfvec[0]);
+    Whalf.append(Whalfvec[1]);
+    Whalf.append(Whalfvec[2]);
+    Whalf.append(Whalfvec[3]);
+#if ndim_ == 3
+    Whalf.append(Whalfvec[4]);
+#endif
+    return Whalf;
+}
 
 #if ndim_ == 3
 /*! @brief Register the libriemann Python module */
@@ -37,28 +84,9 @@ BOOST_PYTHON_MODULE(libpython_riemann3d)
 BOOST_PYTHON_MODULE(libpython_riemann2d)
 #endif
 {
-#if ndim_ == 3
-    class_<StateVector>("StateVector",
-                        init<double, double, double, double, double>())
-            .def("rho", &StateVector::rho)
-            .def("vx", &StateVector::vx)
-            .def("vy", &StateVector::vy)
-            .def("vz", &StateVector::vz)
-            .def("p", &StateVector::p)
-            .def("set", static_cast<void (StateVector::*)(
-                                double, double, double, double, double)>(
-                                &StateVector::set));
-#else
-    class_<StateVector>("StateVector", init<double, double, double, double>())
-            .def("rho", &StateVector::rho)
-            .def("vx", &StateVector::vx)
-            .def("vy", &StateVector::vy)
-            .def("p", &StateVector::p)
-            .def("set",
-                 static_cast<void (StateVector::*)(double, double, double,
-                                                   double)>(&StateVector::set));
-#endif
-
     class_<ExactRiemannSolver>("ExactRiemannSolver", init<double>())
-            .def("solve", &ExactRiemannSolver::solve);
+            .def("solve", &python_solve<ExactRiemannSolver>);
+
+    class_<TRRSSolver>("TRRSSolver", init<double>())
+            .def("solve", &python_solve<TRRSSolver>);
 }

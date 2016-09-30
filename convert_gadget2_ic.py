5e104054a2c6433488fcc5fc945fa6c4483af594
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2016-09-30 14:35:30 +0100

    Finished Gadget IC conversion script.

--- convert_gadget2_ic.py
+++ convert_gadget2_ic.py
@@ -22,12 +22,18 @@
 # Gadget2 simulation, and converts it into an initial condition file and
 # parameter file that can be used with Shadowfax.
 
-# First: check if we have h5py. Without it, we cannot work.
+# First: check if we have h5py and numpy. Without them, we cannot work.
 try:
   import h5py
 except ImportError:
   print "It seems you do not have h5py. However, this script cannot operate " \
         "without it. Please install h5py and try again."
+  exit()
+try:
+  import numpy as np
+except ImportError:
+  print "It seems you do not have numpy. However, this script cannot operate " \
+        "without it. Please install numpy and try again."
   exit()
 
 # Now make sure we have the necessary command line arguments: the name of the
@@ -39,11 +45,18 @@ parser = argparse.ArgumentParser(
 parser.add_argument("icfile", help = "Gadget2 initial condition file (using " \
                                      "snapshot format 3 (HDF5)")
 parser.add_argument("paramfile", help = "Gadget2 parameter file")
+parser.add_argument("--no_questions_asked", action = "store_true",
+                    help = "Do not prompt for user and use default values " \
+                           "for everything")
 
 args = parser.parse_args()
 
 icfile_name = args.icfile
 paramfile_name = args.paramfile
+if args.no_questions_asked:
+  user_input = False
+else:
+  user_input = True
 
 # check if the files exist and can be opened for reading
 import os.path
@@ -123,45 +136,107 @@ else:
         "and propose a BoxSize based on this."
   boxsize = 0.
 
-# we need to ask some questions:
+# we need to ask some questions. However, if user_input is False, we do not
+# ask them and just use the default values.
+
+# default values:
 shadowfax_paramname = paramfile_name.replace(".param", "_shadowfax.ini")
-input = raw_input("Enter a name for the Shadowfax parameter file (default: " \
-                  "\"{shadowfax_paramname}\"): ".format(
-                    shadowfax_paramname = shadowfax_paramname))
-if input:
-  shadowfax_paramname = input
-  if not shadowfax_paramname.endswith(".ini"):
-    shadowfax_paramname += ".ini"
-print "Shadowfax parameter file name will be \"{shadowfax_paramname}\"".format(
-        shadowfax_paramname = shadowfax_paramname)
-
 shadowfax_icname = icfile_name.replace(".hdf5", "_shadowfax.hdf5")
-input = raw_input("Enter a name for the Shadowfax initial condition file " \
-                  "(default: \"{shadowfax_icname}\"): ".format(
-                    shadowfax_icname = shadowfax_icname))
-if input:
-  shadowfax_icname = input
-  if not shadowfax_icname.endswith(".hdf5"):
-    shadowfax_icname += ".hdf5"
-print "Shadowfax initial condition file name will be " \
-      "\"{shadowfax_icname}\"".format(
-        shadowfax_icname = shadowfax_icname)
-
 snapshot_prefix = "snapshot_"
-input = raw_input("Enter a prefix for the snapshot files that will be " \
-                  "written out during the simulation (default: " \
-                  "\"{snapshot_prefix}\"): ".format(
-                    snapshot_prefix = snapshot_prefix))
-if input:
-  snapshot_prefix = input
-print "Snapshot prefix will be \"{snapshot_prefix}\"".format(
-        snapshot_prefix = snapshot_prefix)
-
 riemannsolvertype = "Exact"
 adiabatic_index = 5./3.
 gravity_on = "true"
 internal_units = "galactic"
 output_units = "galactic"
+
+# questions:
+if user_input:
+  input = raw_input("Enter a name for the Shadowfax parameter file (default: " \
+                    "\"{shadowfax_paramname}\"): ".format(
+                      shadowfax_paramname = shadowfax_paramname))
+  if input:
+    shadowfax_paramname = input
+    if not shadowfax_paramname.endswith(".ini"):
+      shadowfax_paramname += ".ini"
+  print "Shadowfax parameter file name will be " \
+        "\"{shadowfax_paramname}\"".format(
+          shadowfax_paramname = shadowfax_paramname)
+
+  input = raw_input("Enter a name for the Shadowfax initial condition file " \
+                    "(default: \"{shadowfax_icname}\"): ".format(
+                      shadowfax_icname = shadowfax_icname))
+  if input:
+    shadowfax_icname = input
+    if not shadowfax_icname.endswith(".hdf5"):
+      shadowfax_icname += ".hdf5"
+  print "Shadowfax initial condition file name will be " \
+        "\"{shadowfax_icname}\"".format(
+          shadowfax_icname = shadowfax_icname)
+
+  input = raw_input("Enter a prefix for the snapshot files that will be " \
+                    "written out during the simulation (default: " \
+                    "\"{snapshot_prefix}\"): ".format(
+                      snapshot_prefix = snapshot_prefix))
+  if input:
+    snapshot_prefix = input
+  print "Snapshot prefix will be \"{snapshot_prefix}\"".format(
+          snapshot_prefix = snapshot_prefix)
+
+  if groups["PartType0"]:
+    input = raw_input("Enter the type of Riemann solver you want to use " \
+                      "(possible values: Exact/TRRS/HLLC, default: " \
+                      "\"{riemannsolvertype}\": ".format(
+                        riemannsolvertype = riemannsolvertype))
+    if input:
+      riemannsolvertype = input
+      if not riemannsolvertype in ["Exact", "HLLC", "TRRS"]:
+        print "Error! Unknow Riemann solver type:", riemannsolvertype
+        exit()
+    print "Using Riemann solver type \"{riemannsolvertype}\"".format(
+            riemannsolvertype = riemannsolvertype)
+
+  if groups["PartType0"]:
+    input = raw_input("Enter a value for the adiabatic index (default: " \
+                      "{adiabatic_index}): ".format(
+                        adiabatic_index = adiabatic_index))
+    if input:
+      adiabatic_index = float(input)
+      if adiabatic_index <= 1.:
+        print "Error! Invalid adiabatic index:", adiabatic_index
+        exit()
+    print "Using adiabatic index", adiabatic_index
+
+  input = raw_input("Should the simulation use gravity? (true/false, " \
+                    "default: {gravity_on}) ".format(gravity_on = gravity_on))
+  if input:
+    gravity_on = input
+    if not gravity_on in ["true", "false"]:
+      print "Error! Invalid choice:", gravity_on
+      exit()
+  if gravity_on == "true":
+    print "Running simulation with gravity"
+  else:
+    print "Running simulation without gravity"
+
+  input = raw_input("Enter the internal units that should be used (CGS/SI/" \
+                    "galactic, default: {internal_units}): ".format(
+                      internal_units = internal_units))
+  if input:
+    internal_units = input
+    if not internal_units in ["CGS", "SI", "galactic"]:
+      print "Error! Invalid choice of internal units:", internal_units
+      exit()
+  print "Using internal units:", internal_units
+
+  input = raw_input("Enter the output units that should be used (CGS/SI/" \
+                    "galactic, default: {output_units}): ".format(
+                      output_units = output_units))
+  if input:
+    output_units = input
+    if not output_units in ["CGS", "SI", "galactic"]:
+      print "Error! Invalid choice of output units:", output_units
+      exit()
+  print "Using output units:", output_units
 
 # the hard part: find out how to convert from gadget units to the requested
 # units
@@ -317,3 +392,106 @@ shadowfax_paramfile.write("StellarFeedback = false\n")
 
 # do not write Cooling, StarFormation, StellarFeedback and Cosmology blocks
 # we use default values for these
+
+# find out the dimensions of the box (if necessary)
+if groups["PartType0"]:
+  coords = np.array(icfile["/PartType0/Coordinates"])
+  gmin = np.amin(coords, axis = 0)
+  gmax = np.amax(coords, axis = 0)
+if groups["PartType1"]:
+  coords = np.array(icfile["/PartType1/Coordinates"])
+  dmin = np.amin(coords, axis = 0)
+  dmax = np.amax(coords, axis = 0)
+if groups["PartType0"] and groups["PartType1"]:
+  xmin = np.minimum(gmin, dmin)
+  xmax = np.maximum(gmax, dmax)
+elif groups["PartType0"]:
+  xmin = gmin
+  xmax = gmax
+else:
+  xmin = dmin
+  xmax = dmax
+if boxsize == 0.:
+  boxsize = np.ceil(xmax - xmin)
+print "Box size will be", boxsize
+boxcenter = 0.5*(xmin+xmax)
+print "Box center is currently", boxcenter
+translation = 0.5*boxsize - boxcenter
+print "All particles will be translated by", translation
+
+# write the Shadowfax IC file
+shadowfax_icfile = h5py.File(shadowfax_icname, 'w')
+
+# write Header
+grp = shadowfax_icfile.create_group("/Header")
+header["BoxSize"] = boxsize
+for key, value in header.iteritems():
+  grp.attrs[key] = value
+
+# write Runtime parameters
+grp = shadowfax_icfile.create_group("/RuntimePars")
+grp.attrs["PeriodicBoundariesOn"] = int(params["PeriodicBoundariesOn"])
+
+# write Units
+# since we use the actual units used by Gadget, we do not have to convert any
+# values in the initial condition file
+# the conversions will be done by Shadowfax when it reads in the IC
+grp = shadowfax_icfile.create_group("/Units")
+grp.attrs["Unit length in cgs (U_L)"] = gadget_unit_length_in_cgs
+grp.attrs["Unit mass in cgs (U_M)"] = gadget_unit_mass_in_cgs
+grp.attrs["Unit time in cgs (U_t)"] = gadget_unit_time_in_cgs
+grp.attrs["Unit current in cgs (U_I)"] = 1.
+grp.attrs["Unit temperature in cgs (U_T)"] = 1.
+
+# write Particle group(s)
+if groups["PartType0"]:
+  grp = shadowfax_icfile.create_group("/PartType0")
+  grp.create_dataset("Coordinates",
+                     data = np.array(icfile["/PartType0/Coordinates"]) + \
+                            translation,
+                     dtype = 'd')
+  grp.create_dataset("Velocities",
+                     data = np.array(icfile["/PartType0/Velocities"]),
+                     dtype = 'd')
+  grp.create_dataset("Masses",
+                     data = np.array(icfile["/PartType0/Masses"]),
+                     dtype = 'd')
+  if has_density:
+    grp.create_dataset("Density",
+                       data = np.array(icfile["/PartType0/Density"]),
+                       dtype = 'd')
+  grp.create_dataset("SmoothingLength",
+                     data = np.array(icfile["/PartType0/SmoothingLength"]),
+                     dtype = 'd')
+  grp.create_dataset("InternalEnergy",
+                     data = np.array(icfile["/PartType0/InternalEnergy"]),
+                     dtype = 'd')
+  grp.create_dataset("ParticleIDs",
+                     data = np.array(icfile["/PartType0/ParticleIDs"]),
+                     dtype = 'L')
+
+if groups["PartType1"]:
+  grp.create_dataset("Coordinates",
+                     data = np.array(icfile["/PartType1/Coordinates"]) + \
+                            translation,
+                     dtype = 'd')
+  grp.create_dataset("Velocities",
+                     data = np.array(icfile["/PartType1/Velocities"]),
+                     dtype = 'd')
+  if has_mass:
+    masses = np.array(icfile["/PartType1/Masses"])
+  else:
+    masses = np.zeros(header["NumPart_ThisFile"][1]) + header["MassTable"][1]
+  grp.create_dataset("Masses", data = masses, dtype = 'd')
+  grp.create_dataset("ParticleIDs",
+                     data = np.array(icfile["/PartType0/ParticleIDs"]),
+                     dtype = 'L')
+
+# print some final instructions
+print ""
+print "Initial conditions successfully generated"
+print "Run the simulation using the following command:"
+if groups["PartType0"] and not has_density:
+  print "\t../shadowfax3d --read_mass --params", shadowfax_paramname
+else:
+  print "\t../shadowfax3d --params", shadowfax_paramname
